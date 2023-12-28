caf5e3d3a6d5c036b312fab92e7c9d998bcec08c
Michael Buckley <michaelcbuckley@proton.me>
2023-12-28 08:28:56 -0500

    Added some testing functions to streamline scenario testing

--- py2048/main.py
+++ py2048/main.py
@@ -1,43 +1,65 @@
 # 2048 Main File
 # Currently being used for production testing
 
+# Python Modules
+from random import randint
+
 # Local Modules
 from py2048.classes import GameBoard, GameCell, Direction
 
-def test1(game: GameBoard):
+def convert_string_to_move(input_str: str):
+    conversion_map = {
+        'r': Direction.RIGHT,
+        'u': Direction.UP,
+        'd': Direction.DOWN,
+        'l': Direction.LEFT
+    }
+    return conversion_map[input_str]
+
+def perform_moves(board: GameBoard, moves: list[Direction]) -> None:
+    """
+    Perform a series of moves
+    """
+    print('START')
+    board.print_board()
+
+    for i, move in enumerate(moves):
+        if isinstance(move, str):
+            move = convert_string_to_move(move)
+        print(f'STEP {i+1}')
+        board.move_pieces(move)
+        board.print_board()
+
+def test1(board: GameBoard):
     for i in range(4):
         for j in range(2):
-            game.grid[i][j] = GameCell(j, i, 2)
-    game.grid[0][2] = GameCell(2, 0, 4)
+            value = 2 if j else 4
+            board.set_cell(i, j, value)
+    # game.grid[0][2] = GameCell(2, 0, 4)
     # for _ in range(10):
     #     if not game.generate_tile():
     #         break
-    print('BEFORE')
-    game.print_board()
+    
+    moves = ['r', 'd', 'd', 'd']
 
-    game.move_pieces(Direction.RIGHT)
-    print('STEP 1')
-    game.print_board()
-    print('AFTER')
-    game.move_pieces(Direction.DOWN)
-    game.print_board()
+    perform_moves(board, moves)
 
-def test2(game: GameBoard):
-    game.grid[0][2] = GameCell(2, 0, 8)
+def test2(board: GameBoard):
+    board.grid[0][2] = GameCell(2, 0, 8)
     for _ in range(10):
-        game.generate_tile()
+        board.generate_tile()
     print('BEFORE')
-    game.print_board()
+    board.print_board()
 
     print('UP')
-    game.print_board(game.get_rotated_board(Direction.UP))
+    board.print_board(board.get_rotated_board(Direction.UP))
 
     print('LEFT')
-    game.print_board(game.get_rotated_board(Direction.LEFT))
+    board.print_board(board.get_rotated_board(Direction.LEFT))
 
     print('RIGHT')
-    game.print_board(game.get_rotated_board(Direction.RIGHT))
+    board.print_board(board.get_rotated_board(Direction.RIGHT))
 
 if __name__ == '__main__':
-    game = GameBoard()
-    test1(game)
+    board = GameBoard()
+    test1(board)
