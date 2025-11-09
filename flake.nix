b95a352c2b6614069de93fda2cf2cee04a968634
Michael Buckley <michaelcbuckley@proton.me>
2025-11-08 22:28:31 -0500

    Converted project to pyproject and started adding nix components

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Py2048 Nix Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = {flake-parts, ...} @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+
+      imports = [
+        #./nix/packages.nix
+        ./nix/devshells.nix
+      ];
+    };
+}
